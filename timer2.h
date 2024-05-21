22cf294c3b400110a1fe52d7af0defc3b0b15145
shangcj2 <shangcj2@sany.com.cn>
2024-05-21 10:59:45 +0800

    add timer2.h

---
+++ timer2.h
@@ -0,0 +1,35 @@
+/**
+ * @file time.h
+ * @author shangchangjun
+ * @date 2023/08/10
+ * @brief timer definitions.
+ **/
+//时间类
+#ifndef ITC_TIMER_H
+#define ITC_TIMER_H
+
+#include <chrono>
+
+namespace itc {
+
+namespace utils {
+
+class Timer {
+public:
+    Timer() : _start(std::chrono::steady_clock::now()) {}
+
+    std::chrono::milliseconds stop() {
+        auto cur = std::chrono::steady_clock::now();
+
+        return std::chrono::duration_cast<std::chrono::milliseconds>(cur - _start);
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> _start{};
+};
+
+}
+
+}
+
+#endif // end ITC_TIMER_H
